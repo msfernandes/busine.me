dd4109a1cf9c3bca4844e4287165dfa024296f59
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2015-08-31 13:03:53 -0300

    Added authenticion app
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

---
+++ busineme/authentication/models.py
@@ -0,0 +1,23 @@
+from defaults.models import BusinemeModel
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class RankPosition(BusinemeModel):
+    description = models.CharField(max_length=100)
+    min_points = models.IntegerField()
+
+    def __str__(self):
+        return "{} - {}".format(self.id, self.description)
+
+
+class BusinemeUser(BusinemeModel, AbstractUser):
+    pontuation = models.IntegerField(default=0)
+    rank = models.ForeignKey(RankPosition, null=True)
+
+    def save(self, *args, **kwargs):
+        self.username = self.username.lower()
+        super(BusinemeModel, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return "{} - {} {}".format(self.id, self.username, self.email)
