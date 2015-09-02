d76ed3509acb6a24bfd42f023cb7e9e9e3c9a124
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2015-09-02 12:45:32 -0300

    Added login page
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- busineme/authentication/views.py
+++ busineme/authentication/views.py
@@ -1,3 +1,8 @@
-# from django.shortcuts import render
+from django.views.generic import View
+from django.shortcuts import render_to_response
 
-# Create your views here.
+
+class LoginView(View):
+
+    def get(self, request):
+        return render_to_response('login.html')
