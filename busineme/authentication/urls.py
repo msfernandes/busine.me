8aa5eaa0411f33c470eaac46fde1b538b6d675f1
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2015-09-04 10:05:47 -0300

    Authentication ok
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- busineme/authentication/urls.py
+++ busineme/authentication/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from .views import LoginView
 
 urlpatterns = [
-    url(r'^login/$', LoginView.as_view()),
+    url(r'^login/$', LoginView.as_view(), name='login'),
 ]
