ed7264cab9921e0ddb6dacb4e8fa9220d62fa892
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2015-09-04 10:05:47 -0300

    Authentication ok
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- busineme/authentication/views.py
+++ busineme/authentication/views.py
@@ -1,8 +1,32 @@
 from django.views.generic import View
-from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.shortcuts import render_to_response, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 
 class LoginView(View):
 
     def get(self, request):
-        return render_to_response('login.html')
+        return render_to_response('login.html',
+                                  context=RequestContext(request))
+
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        response = render_to_response('login.html',
+                                      context=RequestContext(request))
+        if user:
+            if user.is_active:
+                login(user)
+                response = redirect('/')
+            else:
+                messages.add_message(request, messages.ERROR,
+                                     "Inactive user.")
+        else:
+            messages.add_message(request, messages.ERROR, "Invalid user")
+
+        return response
