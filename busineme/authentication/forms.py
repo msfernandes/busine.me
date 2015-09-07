e458110d36e127fff3fd6c3903945a5c03e75a35
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2015-09-07 02:44:28 -0300

    Finished register user
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- busineme/authentication/forms.py
+++ busineme/authentication/forms.py
@@ -1,8 +1,9 @@
-from django.forms import ModelForm
+from django import forms
 from .models import BusinemeUser
 
 
-class BusinemeUserForm(ModelForm):
+class BusinemeUserForm(forms.ModelForm):
+
     class Meta:
         model = BusinemeUser
         fields = ('first_name', 'username', 'email', 'password')
