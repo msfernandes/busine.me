7a9ee26cd8d96d99396736c2502d6509ded564c4
Wilton Rodrigues <braynwilton@gmail.com>
2015-10-06 11:26:50 -0300

    Busline url defined

--- busineme/core/urls.py
+++ busineme/core/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
-from .views import BuslineSearchResultView
+from .views import BuslineSearchResultView, BuslineProfileView
 
 urlpatterns = [
     url(r'^search/busline/$', BuslineSearchResultView.as_view(),
         name='search_results'),
-
+    url(r'busline/(?P<line_number>[0-9]+)/$', BuslineProfileView.as_view(),
+        name='busline_profile'),
+    url(r'busline/(?P<line_number>[0-9]+\.[0-9]+)/$',
+        BuslineProfileView.as_view(), name='busline_profile'),
     url(r'^search/', TemplateView.as_view(template_name="search.html"),
         name="search"),
 ]
--- busineme/core/views.py
+++ busineme/core/views.py
@@ -18,3 +18,13 @@ class BuslineSearchResultView(View):
         response = render_to_response("search_result.html", locals(),
                                       context_instance=RequestContext(request))
         return response
+
+
+class BuslineProfileView(View):
+    http_method_names = [u'get']
+
+    def get(self, request, line_number):
+        busline = Busline.api_get(line_number)
+        response = render_to_response("busline_profile.html", locals(),
+                                      context_instance=RequestContext(request))
+        return response
