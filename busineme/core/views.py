596630be06ee31ef05a41520196b6a0c54d1bde7
Carlos Oliveira <carlosoliveira@duzeru.org>
2015-10-07 14:54:36 -0300

    Retrieving user favorite lines
    Signed-off-by: Carlos Oliveira <carlosoliveira@duzeru.org>

--- busineme/core/views.py
+++ busineme/core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.generic import View
-from .models import Busline
+
+from .models import Busline, Favorite
 
 
 class BuslineSearchResultView(View):
@@ -15,6 +16,16 @@ class BuslineSearchResultView(View):
         line_number = request.GET['busline']
         buslines = Busline.api_filter_contains(line_number)
         count_busline = len(buslines)
+
+        context = {}
+
+        if request.user.is_authenticated():
+            user = request.user
+            user_favorites = Favorite.objects.filter(user=user)
+            user_favorites = [favorite.busline for favorite in user_favorites]
+            context = {'user_favorites': user_favorites}
+
         response = render_to_response("search_result.html", locals(),
-                                      context_instance=RequestContext(request))
+                                  context_instance=RequestContext(request,
+                                                                  context))
         return response
