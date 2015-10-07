50ee839b79c6141debc43aa2e16b3014867c58fc
Carlos Oliveira <carlosoliveira@duzeru.org>
2015-10-07 17:27:40 -0300

    Login required to (un)favorite buslines
    Signed-off-by: Carlos Oliveira <carlosoliveira@duzeru.org>

--- busineme/core/urls.py
+++ busineme/core/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
+
 from .views import BuslineSearchResultView, FavoriteBuslineView
 
 urlpatterns = [
@@ -8,6 +10,6 @@ urlpatterns = [
 
     url(r'^search/', TemplateView.as_view(template_name="search.html"),
         name="search"),
-    url(r'^favorite/busline/$', FavoriteBuslineView.as_view(),
+    url(r'^favorite/busline/$', login_required(FavoriteBuslineView.as_view()),
         name="favorite_busline")
 ]
