1b2ee93bd32ba08098c694edc11b960abf0dad20
Carlos Oliveira <carlospecter@gmail.com>
2015-10-26 18:10:48 -0200

    Calculate position observer
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    Signed-off-by: Lucas Severo <lucassalves65@gmail.com>

--- busineme/authentication/models.py
+++ busineme/authentication/models.py
@@ -1,14 +1,18 @@
 from defaults.models import BusinemeModel
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
+from django.dispatch import receiver
+from django.conf import settings
 
 
 class RankPosition(BusinemeModel):
     description = models.CharField(max_length=100)
     min_points = models.IntegerField()
+    max_points = models.IntegerField()
 
     def __str__(self):
-        return "{} - {}".format(self.id, self.description)
+        return "{} - ({}) {}".format(self.id, self.min_points,
+                                     self.description)
 
 
 class BusinemeUser(BusinemeModel, AbstractUser):
@@ -23,3 +27,11 @@ class BusinemeUser(BusinemeModel, AbstractUser):
 
     def __str__(self):
         return "{} - {} {}".format(self.id, self.username, self.email)
+
+
+@receiver(models.signals.post_save, sender=settings.AUTH_USER_MODEL)
+def calculate_position(sender, instance, **kwargs):
+    rank = RankPosition.objects.get(min_points__lte=instance.pontuation,
+                                    max_points__gt=instance.pontuation)
+    sender.objects.filter(id=instance.id).update(rank=rank)
+    instance.refresh_from_db()
